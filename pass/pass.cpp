9844d0e39faf609955c83f4417dd0634baf3889e
VitaNuova <vitanuova0512@gmail.com>
2017-06-06 14:59:31 +0000

    get non-sensitive and protecting functions

--- pass/pass.cpp
+++ pass/pass.cpp
@@ -3,6 +3,7 @@
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/raw_ostream.h"
+#include <cstdlib>
 
 using namespace llvm;
 using namespace std;
@@ -27,27 +28,39 @@ namespace {
            }
         }
       }
-      virtual bool runOnModule(Module& M) {
-         checkParameters(M);
+      
+      vector<string> findNonSensitive(Module& M) {
          Module::FunctionListType& functions = M.getFunctionList(); 
          vector<string> not_sens_funcs;
-         bool found = false;
          for(Function& f: functions) {
            string name = f.getName();
-           for(string sens_func: sfParam) {
-              if(name == sens_func) {
-                 found = true; 
-              }
+           if(find(sfParam.begin(), sfParam.end(), name) == sfParam.end()) {
+              not_sens_funcs.push_back(name);
            }
-           if(found == false) {
-              not_sens_funcs.push_back(name);               
-           }
-           found = false;
          }
-         for(string func: not_sens_funcs) {
-            errs() << func << '\n';
-         }
-      
+         return not_sens_funcs;
+      }
+ 
+      vector<string> findProtectingFuncs(string sens_func, vector<string> not_sens_funcs) {
+         vector<string> protecting_funcs;
+         int rand_num = 0;
+         while(!(protecting_funcs.size() == clParam)) {
+            rand_num = rand() % not_sens_funcs.size(); 
+            if(find(protecting_funcs.begin(), protecting_funcs.end(), not_sens_funcs[rand_num]) == protecting_funcs.end()) {
+               protecting_funcs.push_back(not_sens_funcs[rand_num]);
+            }
+            errs() << sens_func << " will be protected by " << not_sens_funcs[rand_num] << ".\n";
+         }           
+         return protecting_funcs;
+      }
+
+      virtual bool runOnModule(Module& M) {
+         checkParameters(M);
+         srand(time(0));
+         vector<string> not_sens_funcs = findNonSensitive(M);
+         for(string sens_func: sfParam) {
+            vector<string> protecting_funcs = findProtectingFuncs(sens_func, not_sens_funcs);
+         }      
       }
    };
 }
